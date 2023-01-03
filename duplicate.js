6a1e7e807781fbead09af445ee14b629ba3d9ccf
Hezron-tech <kiprophez@gmail.com>
2023-01-03 14:44:28 +0300

    program to remove duplicates

---
+++ duplicate.js
@@ -0,0 +1,11 @@
+var duplicate = function(nums){
+    for (let i=0; i<nums.length;){
+        if (nums[i]=nums[i+1]){
+            nums.splice(i,1)
+        }
+        else{
+            i++
+        }
+    }
+    return nums
+};
